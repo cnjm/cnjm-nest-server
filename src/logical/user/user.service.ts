5daff068058784bc2951a80cf0969cde7685911b
cnjm <1837847333@qq.com>
2021-09-07 11:05:29 +0800

    :sparkles: 新增异常处理过滤器

--- src/logical/user/user.service.ts
+++ src/logical/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/entities/user/User.entity';
 import { Repository } from 'typeorm';
@@ -13,6 +13,7 @@ export class UserService {
    * 账号列表业务逻辑处理
    */
   async getList(): Promise<any> {
+    throw new UnauthorizedException(`Custom error`);
     const qb = this.userRepository.createQueryBuilder('user');
     const data = await qb.getMany();
     return data;
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix(AppModule.bsUrl); // 全局基础URL，届时所有路由前都会有这个基础的url
+
+  // 全局基础URL，届时所有路由前都会有这个基础的url
+  app.setGlobalPrefix(AppModule.bsUrl);
+
+  // 全局注册错误的过滤器
+  // app.useGlobalFilters(new HttpExceptionFilter());
+
   await app.listen(AppModule.port, () => {
     console.log(`http://localhost:${AppModule.port}/${AppModule.bsUrl}`);
   });
